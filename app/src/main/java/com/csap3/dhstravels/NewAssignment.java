840b86c6a582c182ff5c3213c8e0a5b4e8ce9bd8
Donna <donnagraceprince@gmail.com>
2019-10-09 22:41:37 -0700

    updating assignment

--- app/src/main/java/com/csap3/dhstravels/NewAssignment.java
+++ app/src/main/java/com/csap3/dhstravels/NewAssignment.java
@@ -4,11 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EdgeEffect;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 public class NewAssignment extends AppCompatActivity {
 
@@ -16,9 +12,12 @@ public class NewAssignment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_assignment);
+        openlast_page();
     }
-    public void openlast_page(View view){
+
+    public void openlast_page(){
         Intent intent = new Intent(this,Second_Page.class);
         startActivity(intent);
+
     }
 }
